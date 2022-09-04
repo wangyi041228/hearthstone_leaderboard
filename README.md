e7f8eeb4681e087fb81fca88ae148214a53f948e
王益 <18771051812@qq.com>
2022-09-05 04:32:54 +0800

    add search function

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
-Hearthstone Leaderboard Scraper
-炉石传说外服排行榜数据下载器
+Hearthstone Leaderboard Scraper and Searcher  
+炉石传说外服排行榜数据下载器和查询器
 # 使用方式
-下例为以200个线程下载美服酒馆战棋第8赛季的数据。  
-* `main('US', 'battlegrounds', 7, 200)`
+以200个线程下载三服酒馆战棋第8赛季的数据。  
+```
+dump_all('US', 'battlegrounds', 7, 200)
+dump_all('EU', 'battlegrounds', 7, 200)
+dump_all('AP', 'battlegrounds', 7, 200)
+```
+
+查询某服、某ID（可限定MMR）的MMR和排名。
+```
+search_id('US', 'wtybill')
+search_id('US', 'Apple', _mmr=171)
+search_id('US', 'M00NLIGHT')
+```
+查询结果。
+```
+Find 11 result(s) of wtybill: (mmr, rank)
+(9727, 9)
+(7494, 242)
+(4306, 12705)
+(3939, 16982)
+(3232, 28300)
+(2326, 54756)
+(2317, 55084)
+(1168, 119529)
+(607, 170984)
+(514, 182414)
+(316, 204102)
+
+Find 1 result(s) of Apple (mmr=171) : (mmr, rank)
+(171, 237705)
+
+M00NLIGHT not found. Showing result(s) of M00NL1GHT with 89/100 simalarity.
+Find 1 result(s) of M00NL1GHT: (mmr, rank)
+(814, 151210)
+```
+
 # 服务器异常
 查询条件超出范围时会返回：  
 `"rows":[],"pagination":{"totalPages":0,"totalSize":0}`    
@@ -11,15 +45,19 @@ Hearthstone Leaderboard Scraper
 # 特殊说明
 * 数据源几乎实时更新，因为诸多原因（数据量大，回复中无用信息多，网速慢），抓到的数据一定有错有漏。
 * [ez_aio](https://pypi.org/project/ez-aio )是土制封装，本地修改未上传，可自行手搓。
-* 结尾的请求稀烂，浪费不少时间。
 # 下载速度
-以移动宽带WiFi直连（代理会快很多）为例：  
-99线程：
+以移动宽带WiFi直连为例：  
+99线程直连：
   * 欧服20000页用31分钟
   * 美服9000页用14分钟
   * 亚服4000页用5分钟
 
-200线程：
+200线程直连：
   * 欧服21000页用20分钟
   * 美服9000页用9分钟
   * 亚服4100页用3分钟
+
+200线程+不稳定的代理：
+  * 欧服21000页用14-16分钟
+  * 美服9000页用4-8分钟
+  * 亚服4100页用2分钟
--- hsleaderboard.py
+++ hsleaderboard.py
@@ -1,10 +1,29 @@
-from ez_aio import aio, header0
+from ez_aio import aio, header0, proxy0
 from json import loads
 from collections import deque
 from datetime import datetime
+from os import listdir
+from thefuzz import fuzz
 
 flag = True
 total_pages = 1001
+search_data = {
+    'AP': None,
+    'EU': None,
+    'US': None,
+}
+
+# search_data = {
+#     'AP': {
+#         0: {
+#             'name1': {(mmr, rank), (mmr, rank)},
+#             'name2': {(mmr, rank), (mmr, rank)}
+#         },
+#         1: {},
+#     },
+#     'EU': None,
+#     'US': None,
+# }
 
 
 def after(raw):
@@ -21,18 +40,18 @@ def after(raw):
     return flag0, _results
 
 
-def main(_region, _mode, _season, li=99, inc=1000):
+def dump_all(_region, _mode, _season, li=99, inc=1000):
     global flag, total_pages
     start = 1
     total_pages = 1001
     flag = True
     now = datetime.now().strftime('%Y%m%d_%H_%M_%S')
-    fname = f'{_region}_{_mode}_{_season}_{now}.txt'
+    fname = f'data/{_region}/{_mode}/{_season}_{now}.txt'
     while flag:
         urls = [
             f'https://hearthstone.blizzard.com/zh-tw/api/community/leaderboardsData?region={_region}&leaderboardId={_mode}&page={page}&seasonId={_season}'
             for page in range(start, min((start + inc), total_pages + 1))]
-        _r = aio.get(urls=urls, headers=header0, forced=True, li=li, func=after)
+        _r = aio.get(urls=urls, headers=header0, proxy=proxy0, forced=True, li=li, func=after)
         print(start, start + inc - 1, sep=' -> ', end=' : ')
         data = deque()
         for _recode in _r:
@@ -47,10 +66,82 @@ def main(_region, _mode, _season, li=99, inc=1000):
                 print(_item[0], _item[1], _item[2], sep=',', file=f)
 
 
+def dump_pages(_region, _mode, _season, _range, li=99):
+    now = datetime.now().strftime('%Y%m%d_%H_%M_%S')
+    fname = f'data/{_region}/{_mode}/{_season}_{now}.txt'
+    urls = [
+        f'https://hearthstone.blizzard.com/zh-tw/api/community/leaderboardsData?region={_region}&leaderboardId={_mode}&page={page}&seasonId={_season}'
+        for page in _range]
+    _r = aio.get(urls=urls, headers=header0, forced=True, li=li, func=after)
+    data = deque()
+    for _recode in _r:
+        for _item in _recode:
+            data.append(_item)
+    with open(fname, 'a', encoding='utf-8') as f:
+        for _item in data:
+            print(_item[0], _item[1], _item[2], sep=',', file=f)
+
+
+def search_id(_region, _id, _mmr=None, _mode='battlegrounds'):
+    global search_data
+    if search_data[_region] is None:
+        search_data[_region] = {0: {}, 1: {}}
+        files = listdir(f'data/{_region}/{_mode}')
+        if files:
+            fname = f'data/{_region}/{_mode}/{files[-1]}'
+            with open(fname, 'r', encoding='utf-8') as f:
+                for line in f.readlines():
+                    elements = line.split(',')
+                    rank = int(elements[0])
+                    mmr = int(elements[-1])
+                    name = ','.join(elements[1:-1])
+                    if name in search_data[_region][0]:
+                        search_data[_region][0][name].add((mmr, rank))
+                    else:
+                        search_data[_region][0][name] = {(mmr, rank), }
+        for file in files[-2:-5:-1]:
+            fname = f'data/{_region}/{_mode}/{file}'
+            with open(fname, 'r', encoding='utf-8') as f:
+                for line in f.readlines():
+                    elements = line.split(',')
+                    rank = int(elements[0])
+                    mmr = int(elements[-1])
+                    name = ','.join(elements[1:-1])
+                    if name not in search_data[_region][0]:
+                        search_data[_region][0][name] = {(mmr, rank), }
+    if _id in search_data[_region][0]:
+        _id_new = _id
+    else:
+        _simi, _id_new = max((fuzz.ratio(_id, k), k) for k in search_data[_region][0])
+        print(f'{_id} not found. Showing result(s) of {_id_new} with {_simi}/100 simalarity.')
+    if _mmr is None:
+        res = sorted(search_data[_region][0][_id_new], reverse=True)
+        print(f'Found {len(res)} result(s) of {_id_new}: (mmr, rank)')
+        for line in res:
+            print(line)
+    else:
+        res = sorted([x for x in search_data[_region][0][_id_new] if (x[0] == _mmr)], reverse=True)
+        print(f'Found {len(res)} result(s) of {_id_new} (mmr={_mmr}) : (mmr, rank)')
+        for line in res:
+            print(line)
+    print()
+
+
 if __name__ == '__main__':
     # region = US EU AP
     # mode = battlegrounds mercenaries standard classic wild
     # season = 战棋4-8赛季=3-7 佣兵7-11 标准狂野经典103-107
-    main('US', 'battlegrounds', 7, 200)
-    main('EU', 'battlegrounds', 7, 200)
-    main('AP', 'battlegrounds', 7, 200)
+
+    # dump_all usage
+    # dump_all('US', 'battlegrounds', 7, 200)
+    # dump_all('EU', 'battlegrounds', 7, 200)
+    # dump_all('AP', 'battlegrounds', 7, 200)
+
+    # dump_pages usage
+    # dump_pages('US', 'battlegrounds', 7, range(5720, 5850), 300)
+
+    # search_id usage
+    search_id('US', 'wtybill')
+    search_id('US', 'Apple')
+    search_id('US', 'Apple', _mmr=171)
+    search_id('US', 'M00NLIGHT')
